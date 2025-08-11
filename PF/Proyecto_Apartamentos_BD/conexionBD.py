11a8ccb86bf131ca9a733e44d9f7abd3fd350ea1
SinoheGalvan <jose_3141240116@utd.edu.mx>
2025-08-10 19:42:40 -0600

    Add files via upload

---
+++ PF/Proyecto_Apartamentos_BD/conexionBD.py
@@ -0,0 +1,14 @@
+import mysql.connector
+from mysql.connector import Error
+
+def conectarBD():
+    try:
+        conexion1=mysql.connector.connect(host="localhost", user="root", passwd="",database="bd_apartamentos")
+        cursor1=conexion1.cursor()
+        return cursor1, conexion1
+    except Error as e:
+        return None,e
+
+def desconectarBD(conexion):
+    conexion.commit()
+    conexion.close() 
